7d46fe45b471f4c9fbb350cb840f7b922246e081
Chisom-git <chisom94@gmail.com>
2026-07-22 05:17:13 +0100

    shadow-Dom feature

--- cypress.config.js
+++ cypress.config.js
@@ -5,10 +5,11 @@ module.exports = defineConfig({
 
   e2e: {
     baseUrl: 'https://the-internet.herokuapp.com/',
-    defaultCommandTimeout: 60000,
+    defaultCommandTimeout: 80000,
     viewportHeight: 960,
     viewportWidth: 1530,
     chromeWebSecurity: false,
+    includeShadowDom: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
